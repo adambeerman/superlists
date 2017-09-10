662e6e9fc22c209b7584ff2d8ed4d0dce6c63eed
Adam Beerman <adam.beerman@gmail.com>
2017-09-10 14:40:20 -0400

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,11 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-
-	# Add a purposely failing test to confirm that we trust this Django test driver
-	def test_bad_maths(self):
-		self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+	
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
